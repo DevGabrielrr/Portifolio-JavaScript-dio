64275f853d896b4206044bf23cb5f8c9a6946710
Gabriel Ribeiro <bieelrr4@gmail.com>
2024-07-22 11:40:57 -0300

    ajustando api

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/DevGabrielrr/Breadcrumbsdesafio-js-2-portifolio-dio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/DevGabrielrr/desafio-js-2-portifolio-dio/main/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
